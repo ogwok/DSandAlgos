28002967a7d6fd1e8f754a37323c0b40eff13d90
ogwok <williamogwok11@gmail.com>
2020-08-15 11:18:15 +0300

    add shift

--- learningLinkedList/linkedlist.js
+++ learningLinkedList/linkedlist.js
@@ -56,4 +56,15 @@ class LinkedList {
         }
 
     }
+    shift(){
+        if(!this.head) return undefined;
+        let temp = this.head
+        this.head = this.head.next;
+        temp.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return temp;
+    }
 }
