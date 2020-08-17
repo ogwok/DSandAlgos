6b4ee70d6a612af27f8424b26d84e8622badf9c4
ogwok <williamogwok11@gmail.com>
2020-08-17 09:16:13 +0300

    add get

--- learningLinkedList/linkedlist.js
+++ learningLinkedList/linkedlist.js
@@ -67,4 +67,12 @@ class LinkedList {
         }
         return temp;
     }
+    get(index){
+        if (index < 0 || index > this.length) return undefined;
+        let temp = this.head
+        for (let i = 0; i < index; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
 }
