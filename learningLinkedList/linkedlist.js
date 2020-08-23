cf3678c9c764387714d6d237e8c97493a363c9c3
ogwok <williamogwok11@gmail.com>
2020-08-23 06:17:15 +0300

    add insert

--- learningLinkedList/linkedlist.js
+++ learningLinkedList/linkedlist.js
@@ -83,4 +83,16 @@ class LinkedList {
         }
         return false
     }
+    insert (index, value){
+        if (index === 0) return this.unshift(value);
+        if (index === this.length) return this.push(value);
+        if (index < 0 || index >= this.length) return undefined;
+        const newNode = new Node(value);
+        let prev = this.get(index -1);
+        newNode.next = prev.next;
+        prev.next = newNode;
+        this.length++;
+
+        return true;
+    }
 }
