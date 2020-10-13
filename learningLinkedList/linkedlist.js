aed4af62c6e2850211d686aa435bdd167c73b243
ogwok <williamogwok11@gmail.com>
2020-10-13 12:15:11 +0300

    add pop

--- learningLinkedList/linkedlist.js
+++ learningLinkedList/linkedlist.js
@@ -24,4 +24,22 @@ class LinkedList {
 
         return this;
     }
+    pop(){
+        if(!this.head) return undefined;
+        let temp = this.head;
+        let pre = this.head;
+        while(temp.next){
+            pre = temp;
+            temp = temp.next;
+        }
+        this.tail = pre;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+
+        return temp;
+    }
 }
