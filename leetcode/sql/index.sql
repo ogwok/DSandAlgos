91d621ea830407e6e4014b4cc30d3452df8cccdd
ogwok <williamogwok11@gmail.com>
2021-03-12 09:00:00 +0300

    leet 1757

--- leetcode/sql/index.sql
+++ leetcode/sql/index.sql
@@ -3,3 +3,6 @@ SELECT product_id FROM Products WHERE low_fats = "Y" AND recyclable = "Y"
 
 -- 584. Find Customer Referee
 SELECT name FROM Customer WHERE referee_id != 2 OR referee_id IS NULL
+
+-- 595. Big Countries
+SELECT name, population, area FROM World WHERE population >= 25000000 OR area >= 3000000 
