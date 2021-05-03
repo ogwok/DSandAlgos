efbb2a7b5a045267e1ce235d1bbf9d0d9a5630e4
ogwok <williamogwok11@gmail.com>
2021-05-03 08:24:00 +0300

    leet 1683

--- leetcode/sql/index.sql
+++ leetcode/sql/index.sql
@@ -9,3 +9,6 @@ SELECT name, population, area FROM World WHERE population >= 25000000 OR area >=
 
 -- 1148. Article Views I
 SELECT DISTINCT author_id AS id FROM Views WHERE viewer_id = author_id ORDER BY id ASC
+
+-- 1683. Invalid Tweets
+SELECT tweet_id FROM Tweets WHERE length(content)>15
