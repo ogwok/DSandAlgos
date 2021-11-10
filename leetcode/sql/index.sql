643b1e18e1f004ed4c723444676837c6d681537f
ogwok <williamogwok11@gmail.com>
2021-11-10 11:00:00 +0300

    leet 1757

--- leetcode/sql/index.sql
+++ leetcode/sql/index.sql
@@ -1,2 +1,5 @@
 -- 1757. Recyclable and Low Fat Products
 SELECT product_id FROM Products WHERE low_fats = "Y" AND recyclable = "Y"
+
+-- 584. Find Customer Referee
+SELECT name FROM Customer WHERE referee_id != 2 OR referee_id IS NULL
